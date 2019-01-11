416bab997bb6efa442a7e66a09abd3c511e8e4cc
Valerie <vfalcone7070@gmail.com>
2019-01-11 01:38:09 +0000

    Finished version of project

--- public/bundle.js
+++ public/bundle.js
@@ -17598,8 +17598,25 @@ __webpack_require__.r(__webpack_exports__);
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 
 
-window.onload = function () {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generatorExcuse();
+  console.log("Hello Rigo from the console! ");
+};
+
+let generatorExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pinecone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possesion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possesionIndx = Math.floor(Math.random() * possesion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return pronoun[proIndx] + " " + subject[subjIndx] + " " + action[actionIndx] + " " + possesion[possesionIndx] + " " + where[whereIndx];
 };
 
 /***/ }),
